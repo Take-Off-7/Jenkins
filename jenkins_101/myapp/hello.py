d45a6283b6591fb71afc28e13a65864210740337
Take-Off-7 <tfakeye7@gmail.com>
2025-09-04 12:36:54 +0000

    update hello.py

--- jenkins_101/myapp/hello.py
+++ jenkins_101/myapp/hello.py
@@ -1,15 +1,11 @@
-import sys
+# hello.py
+import argparse
 
-def main(name="World"):
+def main(name):
     print(f"Hello, {name}!")
 
 if __name__ == "__main__":
-    # Check for --name argument
-    if "--name" in sys.argv:
-        index = sys.argv.index("--name") + 1
-        if index < len(sys.argv):
-            main(sys.argv[index])
-        else:
-            main()
-    else:
-        main()
+    parser = argparse.ArgumentParser(description="Greet someone.")
+    parser.add_argument('--name', default='World', help='Name to greet')
+    args = parser.parse_args()
+    main(args.name)
